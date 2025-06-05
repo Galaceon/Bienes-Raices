0ce749ab6c19773815a95a8fc0b3e492465f7cc1
Galaceon <antoniogarcialtr@gmail.com>
2025-06-05 23:53:28 +0200

    dinamic announcement finshed

--- anuncio.php
+++ anuncio.php
@@ -1,45 +1,49 @@
 <?php
-    declare(strict_types= 1);
+    // Base de Datos
+    require 'includes/config/database.php';
+    $db = conectarDB();
 
+    // Obtener ID de cada Propiedad
+    $id = $_GET['id'];
+    $id = filter_var($id, FILTER_VALIDATE_INT);
+
+    if(!$id) {
+        header('location: index.php');
+    }
+
+    // Consulta a la DB
+    $query = "SELECT * FROM propiedades WHERE id = $id";
+    $resultadoConsulta = mysqli_query($db, $query);
+    $propiedad = mysqli_fetch_assoc($resultadoConsulta);
+
+    // Header
     require 'includes/funciones.php';
     incluirTemplate('header');
 ?>
 
     <main class="contenedor seccion contenido-centrado">
-        <h1>Casa en  Venta frente al bosque</h1>
+        <h1> <?php echo $propiedad['titulo']; ?> </h1>
 
-        <img src="build/img/destacada.webp" alt="anuncio image">
+        <img src="imagenes/<?php echo $propiedad['imagen'] ?>" alt="anuncio image">
 
-        <p class="precio">$3.000.000</p>
+        <p class="precio"><?php echo $propiedad['precio']; ?></p>
 
         <ul class="iconos-caracteristicas">
             <li>
                 <img class="icono" loading="lazy" src="build/img/icono_wc.svg">
-                <p>3</p>
+                <p><?php echo $propiedad['wc']; ?></p>
             </li>
             <li>
                 <img class="icono" loading="lazy" src="build/img/icono_estacionamiento.svg">
-                <p>3</p>
+                <p><?php echo $propiedad['estacionamiento']; ?></p>
             </li>
             <li>
                 <img class="icono" loading="lazy" src="build/img/icono_dormitorio.svg">
-                <p>3</p>
+                <p><?php echo $propiedad['habitaciones']; ?></p>
             </li>
         </ul>
 
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit molestias quaerat voluptatibus
-            dicta unde, recusandae praesentium, reiciendis dolorum iste voluptates dolore velit in. Ullam
-            dicta harum rem. Itaque, dolor perspiciatis. Lorem ipsum dolor sit, amet consectetur adipisicing
-            elit. Iusto dignissimos quasi autem impedit eaque deserunt tenetur, voluptatibus id praesentium sit
-            nam culpa eveniet, facere architecto non, exercitationem ipsum cum repellendus. Lorem ipsum dolor sit, amet
-            consectetur adipisicing elit. Eius nisi enim vitae voluptas quidem. Minima non esse molestias sit delectus nulla,
-            repellendus neque eligendi, laborum fugit nisi dolor numquam alias.
-        </p>
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum accusantium sapiente perspiciatis. Deserunt magnam
-            inventore quam cupiditate facere, corrupti, praesentium ipsum id repellat porro doloremque totam culpa in corporis
-            quas.
-        </p>
+        <p> <?php echo $propiedad['descripcion']; ?> </p>
     </main>
 
 <?php incluirTemplate('footer'); ?>
